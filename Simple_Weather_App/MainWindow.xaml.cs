dd1f2f72999293055f4f125b002400bab26f9983
jarodiking <jarodiking17@gmail.com>
2023-09-05 18:59:05 +0200

    Add project files.

---
+++ Simple_Weather_App/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+
+
+namespace Simple_Weather_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Call the API here
+            await CallApiAsync();
+        }
+
+        private async Task CallApiAsync()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string apiUrl = "https://api.example.com/data"; // Replace with your API endpoint URL
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+
+                        // Process the API response data here
+                        // Update your UI with the data, if necessary
+                    }
+                    else
+                    {
+                        // Handle API error here
+                        MessageBox.Show("API request failed with status code: " + response.StatusCode);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions here
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
